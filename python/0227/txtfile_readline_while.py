1a6acc8fbc04855078cd0b015f3990a63a3983b3
unknown <ddongse55@gmail.com>
2026-02-27 17:36:52 +0900

    2026.02.27

--- python/0227/txtfile_readline_while.py
+++
@@ -1,6 +0,0 @@
-with open('hello.txt', 'r', encoding='utf8') as file:
-    line = None
-    while line != '':
-        line = file.readline()
-        print(line.strip('\n')) # 줄바꿈이 한 번만 일어날 수 있도록
-                                # print(line, end = '')과 동일한 역할을 함
--- python/0227/txtfile_writelines_with.py
+++
@@ -1,4 +0,0 @@
-﻿lines = ['안녕하세요\n', '파이썬\n', '도장입니다.\n']
-
-with open('hello.txt', 'w', encoding='utf8') as file:
-    file.writelines(lines)
